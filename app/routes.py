47d2e537a2bbe6b97856617d09f21b1686b68029
Ganesh Balaji <ganeshkbalaji@ymail.com>
2018-06-12 23:28:08 -0700

    added user object

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,13 @@ from app import app
 @app.route('/')
 @app.route('/helloWorld')
 def helloWorld():
-    return "Hello World!"
+    user = {'username': 'ganeshkb'}
+    return '''
+    <html>
+        <head>
+            <title>Microblog</title>
+        </head>
+        <body>
+            <h1>Hi, ''' + user['username'] + '''</h1>
+        </body>
+    </html>'''
